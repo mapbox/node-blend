73bf84ca1a1ea719376c1835836051579e76fbab
Dane Springmeyer <dane@dbsgeo.com>
2013-01-07 15:20:21 -0800

    cleanup unused code

--- src/hextree.hpp
+++ src/hextree.hpp
@@ -26,15 +26,6 @@
 // mapnik
 #include "palette.hpp"
 
-// boost
-// #include <boost/utility.hpp>
-// #include <boost/version.hpp>
-// #include <boost/unordered_map.hpp>
-// #if BOOST_VERSION >= 104600
-// #include <boost/range/algorithm.hpp>
-// #endif
-// #include <boost/scoped_ptr.hpp>
-
 // stl
 #include <vector>
 #include <iostream>
@@ -262,13 +253,8 @@ public:
             int dist, newdist, dist_add;
 
             // find closest match based on mean of r,g,b,a
-#if BOOST_VERSION >= 104600
-            std::vector<rgba>::const_iterator pit =
-                boost::lower_bound(sorted_pal_, c, rgba::mean_sort_cmp());
-#else
             std::vector<rgba>::const_iterator pit =
                 std::lower_bound(sorted_pal_.begin(),sorted_pal_.end(), c, rgba::mean_sort_cmp());
-#endif
             ind = pit-sorted_pal_.begin();
             if (ind == sorted_pal_.size())
                 ind--;
@@ -343,11 +329,7 @@ public:
         create_palette_rek(sorted_pal_, root_.get());
 
         // sort palette for binary searching in quantization
-#if BOOST_VERSION >= 104600
-        boost::sort(sorted_pal_, rgba::mean_sort_cmp());
-#else
         std::sort(sorted_pal_.begin(), sorted_pal_.end(), rgba::mean_sort_cmp());
-#endif
         // returned palette is rearranged, so that colors with a<255 are at the begining
         pal_remap_.resize(sorted_pal_.size());
         palette.clear();
