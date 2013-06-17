2f01d8c385c19a44539d4f29cd039825cb7d68d6
Dane Springmeyer <dane@dbsgeo.com>
2013-06-17 15:32:30 -0700

    read from file into imagemagick - refs same problem as mapbox/tilelive-vector#3

--- test/support/utilities.js
+++ test/support/utilities.js
@@ -31,9 +31,10 @@ exports.imageEqualsFile = function(buffer, file, meanError, callback) {
     if (!image_magick_available) {
         throw new Error("imagemagick 'compare' tool is not available, please install before running tests");
     }
-    var compare = spawn('compare', ['-metric', 'MAE', '-', file, '/dev/null' ]);
     var type = path.extname(file);
     var result = path.join(path.dirname(file), path.basename(file, type) + '.result' + type);
+    var compare = spawn('compare', ['-metric', 'MAE', result, file, '/dev/null' ]);
+    fs.writeFileSync(result, buffer);
 
     var error = '';
     compare.stderr.on('data', function(data) {
@@ -46,7 +47,6 @@ exports.imageEqualsFile = function(buffer, file, meanError, callback) {
 
         var similarity = parseFloat(error.match(/^\d+(?:\.\d+)?\s+\((\d+(?:\.\d+)?)\)\s*$/)[1]);
         if (similarity > meanError) {
-            fs.writeFileSync(result, buffer);
             var err = new Error('Images not equal: ' + error.trim() + ':\n' + result + '\n'+file);
             err.similarity = similarity;
             callback(err);
