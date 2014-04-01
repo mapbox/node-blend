53dbd06af1a704684efe35bc337fa6bededd6c5e
Young Hahn <young@developmentseed.org>
2014-04-01 01:10:55 -0400

    Go for some publish action. [publish binary]

--- test/travis-publish.sh
+++ test/travis-publish.sh
@@ -14,8 +14,6 @@ COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 if test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE"
     then
 
-    exit 0
-
     npm install aws-sdk
     ./node_modules/.bin/node-pre-gyp package testpackage
     ./node_modules/.bin/node-pre-gyp publish info
